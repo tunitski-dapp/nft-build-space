49b392280fbb101ff6f9d8e5b7be444f41e9855f
Uladzislau Tunitski <uladzislau.tunitski@non.keysight.com>
2022-05-17 17:09:00 +0300

    init hardhat

---
+++ contracts/MyEpicNFT.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.4;
+
+import "hardhat/console.sol";
+
+contract MyEpicNFT {
+    constructor() {
+        console.log("I'm a Bird Motherfucker, I'm a Bird!");
+    }
+}
