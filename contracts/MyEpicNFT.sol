ea9f56eeac4b026e949c0445bb45ce1fb000839b
Uladzislau Tunitski <uladzislau.tunitski@non.keysight.com>
2022-05-17 18:57:25 +0300

    create simple nft

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -2,10 +2,27 @@
 
 pragma solidity ^0.8.4;
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-contract MyEpicNFT {
-    constructor() {
+contract MyEpicNFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721 ("SquareNFT", "SQUARE") {
         console.log("I'm a Bird Motherfucker, I'm a Bird!");
+    }
+
+    function makeAnEpicNFT() public {
+        uint256 newItemId = _tokenIds.current();
+
+        _safeMint(msg.sender, newItemId);
+        _setTokenURI(newItemId, "https://jsonkeeper.com/b/DJI3");
+
+        _tokenIds.increment();
+
+        console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
     }
 }
